694d865ba688f6ff8372689080ee6c0a34f4fe68
Josefin Ström <hedvigjosefin@gmail.com>
2020-12-08 11:19:53 +0100

    Change to Imgur API (still no work)

--- src/api/ImgurSource.js
+++ src/api/ImgurSource.js
@@ -2,8 +2,8 @@ import myHeaders from "../api/apiConfig";
 
 
 // tutorial version
-const BASE_URL = "http://sunset.nada.kth.se:8080/iprog/group/58/";   // the DH2642 proxy server
-const API_KEY = "3d2a031b4cmsh5cd4e7b939ada54p19f679jsn9a775627d767";
+//const BASE_URL = "http://sunset.nada.kth.se:8080/iprog/group/58/";   // the DH2642 proxy server
+//const API_KEY = "3d2a031b4cmsh5cd4e7b939ada54p19f679jsn9a775627d767";
 
 function handleHTTPError(response) {
     if (response.ok)
@@ -12,38 +12,39 @@ function handleHTTPError(response) {
 }
 
 
-function apiCall( params ) {
-    /*return fetch("https://api.imgur.com/3/" + params, requestOptions)
+function apiCall( params, requestOptions ) {
+    return fetch("https://api.imgur.com/3/" + params, requestOptions)
         .then(handleHTTPError)
         .then(response => response.json());
-        */
-    return fetch(BASE_URL + params, {
+        
+    /*return fetch(BASE_URL + params, {
         "method": "GET",
         "headers": {
             'X-Mashape-Key': API_KEY
         }
     })
     .then(handleHTTPError)
-    .then(response => response.json());
+    .then(response => response.json());*/
 }
 
 export default function searchImages( query ) {
-   /* let formdata = new FormData();
+    let formdata = new FormData();
 
     let requestOptions = {
         method: 'GET',
         headers: myHeaders,
         body: formdata,
         redirect: 'follow'
-    };*/
+    };
 
+    //let searchString;
+    //(!query) ? searchString = "" : searchString = ("query=" + query);
+    //return apiCall("recipes/search?" + new URLSearchParams(searchString)).then(data => (data.results));
     let searchString;
-    (!query) ? searchString = "" : searchString = ("query=" + query);
-    return apiCall("recipes/search?" + new URLSearchParams(searchString)).then(data => (data.results));
-    /*let searchString = "gallery/search/?";
-    (!query) ? searchString += "" : searchString += "q=" + query;
+    (!query) ? searchString = "" : searchString = "q=" + query;
 
-    return apiCall(searchString, requestOptions).then(data => (data.results));*/
+    return apiCall("gallery/search/?" + new URLSearchParams(searchString), requestOptions)
+    .then(data => (data.results));
 
 }
 
