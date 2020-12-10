e7d075bf75ff1386b459ff36ba38892cd88c2d95
Alexander Själander <alesja@kth.se>
2020-12-10 16:10:07 +0100

    New App New Index commit might not work

---
+++ index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// De verkar vilja att man stoppar in reduxgrejerna här så har dem här tillfälligt ELLER INTE abort
+
+// redux
+
+import { createStore } from "redux";
+
+
+
+import { Provider } from "react-redux";
+
+import { allReducers } from "./redux-model/reducers";
+//console.log(counter); importeras rätt
+
+
+
+
+let store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+console.log(store.getState());
+
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+    
+      <App />
+    
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
