b6b8f425aa12a2b39cb569b726edab499a56eb69
Josefin Ström <hedvigjosefin@gmail.com>
2020-12-12 17:58:06 +0100

    CSS highscore wip
    Is a bit messy

--- src/views/HighScoreView.js
+++ src/views/HighScoreView.js
@@ -4,23 +4,33 @@ const HighScoreView = ({props, nav, scores, addScore}) => {
       <p>
         <button onClick = {()=>nav[0]()}>Back</button>
       </p>
+      
+      <h1 class="highscore">PictoSwap</h1>
       <div class="container">
-        <p>
-          <button onClick = {()=>nav[1]()}>Continue</button>
-        </p>
-          <div> 
-            <table>
-              <tbody>
-                {scores && scores.map(scoreElement => (
-                <tr key={scoreElement.id}>
-                  <td>{scoreElement.name}</td><td>{scoreElement.score}</td>
-                </tr>
-                ))}
-              </tbody>
-            </table> {// Här ska stuff vara sen}
-            }
-          <button onClick = {()=>addScore()}>:)</button>
-        </div>
+        <div class="highscore-container">
+          <p>
+            <button class="highscore" onClick = {()=>nav[1]()}>Continue</button>
+          </p>
+          <h2>High Scores</h2>
+            <div class="highscore-list"> 
+              <table class="highscore">
+                <tbody class="highscore">
+                  <tr class="highscore">
+                  <th class="highscore">Name: </th>
+                  <th class="highscore">Time:</th>
+                  </tr>
+                  
+                  {scores && scores.map(scoreElement => (
+                  <tr class="highscore" key={scoreElement.id}>
+                    <td class="highscore">{scoreElement.name}</td><td>{scoreElement.score}</td>
+                  </tr>
+                  ))}
+                </tbody>
+              </table> {// Här ska stuff vara sen}
+              }
+            <button class="highscore" onClick = {()=>addScore()}>:)</button>
+            </div>
+          </div>
       </div>
     </div>
     
