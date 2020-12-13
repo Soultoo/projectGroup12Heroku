e77bb0d22d05e6b2f08306c0ce5789058b658a34
Josefin Ström <hedvigjosefin@gmail.com>
2020-12-13 18:15:11 +0100

    WIP CSS

--- src/views/SetUpGameView.js
+++ src/views/SetUpGameView.js
@@ -7,51 +7,53 @@ const SetUpGameView = ({nav, dispatch, searchAlbums, query, searchResults, setSe
 
   return (
     <div>
-   
       <button class="back" onClick = {()=>nav[0]()}>Back</button>
       <div class="container">
-      <div class="header">
-        <h1 class="setupgame">PictoSwap</h1>
+        <div class="header">
+          <h1 class="setupgame">PictoSwap</h1>
+        </div>
+      
+        <div class="search searchgrid">
+          <div class="searchmain">
+            <form >
+              <input class="search" onBlur={function(event) {setSearchQuery(event.target.value);}} placeholder={"Example: Cats"}>
+                </input>
+            </form>
+          </div>
+
+          <div class="searchright">
+            <button onClick={()=>promiseAction(
+              dispatch,
+              searchAlbums(query), // FIXA
+              "SETSEARCHRESULTS") // FIXA
+              }>Search!</button>
+          </div>
+
+
+          <div class="album searchbulk">
+
+            { // Måste förhindra att man väljer videor
+                searchResults.filter(x=>(
+                  x.images && 
+                  !(x.images[0].animated) && 
+                  !(x.images[0].height > x.images[0].width
+                    )) ? true : false).map(x=><img 
+                    src={x.images[0].link}
+                    alt="wow"
+                    width="100px"
+                    onClick={()=>setPhotoURL(x.images[0].link)}
+                    style={{cursor:"pointer"}}
+                    ></img>)
+              }
+          </div>
+          
+        </div>
+        <div class="footer">
+          
+          <button class="start" onClick = {()=>nav[1]()}>Start game!</button>
+        
+        </div>
       </div>
-      <p>
-        <button class="start" onClick = {()=>nav[1]()}>Start game!</button>
-      </p>
-    
-      <div class="search searchgrid">
-        <div class="searchmain">
-          <form >
-            <input class="search" onBlur={function(event) {setSearchQuery(event.target.value);}} placeholder={"Example: Cats"}>
-              </input>
-          </form>
-        </div>
-
-        <div class="searchright">
-          <button onClick={()=>promiseAction(
-            dispatch,
-            searchAlbums(query), // FIXA
-            "SETSEARCHRESULTS") // FIXA
-            }>Search!</button>
-        </div>
-
-
-        <div id="album">
-
-          { // Måste förhindra att man väljer videor
-              searchResults.filter(x=>(
-                x.images && 
-                !(x.images[0].animated) && 
-                !(x.images[0].height > x.images[0].width
-                  )) ? true : false).map(x=><img 
-                  src={x.images[0].link}
-                  alt="wow"
-                  width="100px"
-                  onClick={()=>setPhotoURL(x.images[0].link)}
-                  style={{cursor:"pointer"}}
-                  ></img>)
-            }
-        </div>
-      </div>
-    </div>
     </div>
   )
 }
